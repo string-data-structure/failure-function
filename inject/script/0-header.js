7cc30c4db5835b5eca93c5154071b49380248544
make-github-pseudonymous-again <make-github-pseudonymous-again@users.noreply.github.com>
2021-05-28 07:37:50 +0000

    Deploying to gh-pages from @ string-data-structure/failure-function@07f6559082621ab3e9f1bddc9ff14b7e060588cb 🚀

--- inject/script/0-header.js
+++ inject/script/0-header.js
@@ -17,7 +17,8 @@ domReady(() => {
 	header.insertBefore(projectname, header.firstChild);
 
 	const testlink = document.querySelector('header > a[data-ice="testLink"]');
-	testlink.href = 'https://app.codecov.io/gh/string-data-structure/failure-function';
+	testlink.href =
+		'https://app.codecov.io/gh/string-data-structure/failure-function';
 	testlink.target = '_BLANK';
 
 	const searchBox = document.querySelector('.search-box');
